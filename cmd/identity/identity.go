57a56638bc4b386889cbc5eca1962915cd5f779a
grarich <grarich@grawlily.com>
2023-11-05 03:22:56 +0900

    feat: テナントIDを設定するコマンドを追加

--- cmd/identity/identity.go
+++ cmd/identity/identity.go
@@ -9,27 +9,35 @@ import (
 	"conoha_cli/internal/utils"
 	"fmt"
 	"github.com/spf13/cobra"
-	"os"
 )
 
 var (
 	endpoint string
+	tenantID string
 	Cmd      = &cobra.Command{
 		Use:   "identity",
 		Short: "Identity API を操作します。",
 		Long: `Identityの機能を使用できます。
 バージョン情報取得、トークン発行が可能です。`,
 		PreRunE: func(cmd *cobra.Command, args []string) error {
-			if !utils.IsValidUrl(endpoint) {
+			if endpoint != "" && !utils.IsValidUrl(endpoint) {
 				return fmt.Errorf("不正なURLです。 URL: %+v", endpoint)
 			}
 			return nil
 		},
 		RunE: func(cmd *cobra.Command, args []string) error {
-			if err := apps.SetConfig("endpoint.identity", endpoint); err != nil {
-				return err
+			if endpoint != "" {
+				if err := apps.SetConfig("endpoint.identity", endpoint); err != nil {
+					return err
+				}
+				cmd.Println("エンドポイントを設定しました。")
 			}
-			cmd.Println("エンドポイントを設定しました。", os.Stdout)
+			if tenantID != "" {
+				if err := apps.SetConfig("identity.tenant_id", tenantID); err != nil {
+					return err
+				}
+				cmd.Println("テナントを設定しました。")
+			}
 			return nil
 		},
 	}
@@ -42,5 +50,11 @@ func init() {
 		"",
 		`identityのAPIを使用する前にエンドポイントの設定をする必要があります。
 ConoHaのダッシュボードにログインし、エンドポイントを確認してください。`)
+	Cmd.Flags().StringVar(
+		&tenantID,
+		"set-tenant-id",
+		"",
+		`テナントIDを設定します。
+ConoHaのダッシュボードにログインし、テナントIDを確認してください。`)
 	Cmd.AddCommand(GetTokenCmd)
 }
